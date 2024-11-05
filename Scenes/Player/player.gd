848b4f389411468019a76739c40706dd3b26bf9f
kashi618 <nialj618@gmail.com>
2024-11-05 12:25:26 +0000

    Added Double Jump
    Added double jump functionality !

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_POWER = -500.0
+const JUMP_POWER = -350.0
+const MAXJUMPS = 2
 
 
 func _physics_process(delta: float) -> void:
@@ -10,19 +11,32 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Gets input from user
-	getMovement()
+	movement() # Controls going left, right
+	doubleJump() # Controls jump and double jump
 	
 	# Allows movement of character
 	move_and_slide()
 
-func getMovement():
+func movement():
 	# Moves player left and right
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func doubleJump():
+	var numJumps = 0
 	
 	# Jump!
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		print("Jumped")
+		numJumps += 1
 		velocity.y = JUMP_POWER
+	
+	if Input.is_action_just_pressed("jump") and (numJumps < MAXJUMPS) and not is_on_floor():
+		print("DoubleJumped")
+		velocity.y = JUMP_POWER+50
+	
+	if is_on_floor():
+		numJumps = 0
