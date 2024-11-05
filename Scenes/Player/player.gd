1730afd1623a826f08df3f61ce06457e824daf7e
kashi618 <nialj618@gmail.com>
2024-11-05 00:13:23 +0000

    Refactored Character Movement

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_POWER = -500.0
 
 
 func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	# Gets input from user
+	getMovement()
+	
+	# Allows movement of character
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func getMovement():
+	# Moves player left and right
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	
+	# Jump!
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_POWER
