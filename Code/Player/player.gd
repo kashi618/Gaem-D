6ad3f42ab652dc5f6b0cbe7b36bf2fa3fe1f6a24
osomeefm <eamonn.moore@outlook.com>
2024-11-08 20:14:41 +0000

    EventsBus, Player acceleration, walljump, player death,
    Added an events bus to conveniently send project wide signals, changed player movement to use acceleration, added the player wall jump and added a death area which causes player to "die" but no handling for death implemented yet.

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -4,11 +4,18 @@ extends CharacterBody2D
 @onready var coyote_timer: Timer = $CoyoteTimer
 
 @export var double_jump: bool 
+@export var wall_jump: bool 
 const SPEED = 300.0
 const JUMP_POWER = -350.0
+const WALL_JUMP_POWER = 300.00
 const MAXJUMPS = 1
+const ACCELERATION = 70.00
+const DECELERATION = 20.00
 var numJumps = 0
+var can_move = true
 
+func _ready():
+	EventsBus.PlayerDied.connect(_on_PlayerDied)
 
 func _physics_process(delta: float) -> void:
 	# Gravity
@@ -16,8 +23,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Gets input from user
-	movement() # Controls going left, right
-	doubleJump() # Controls jump and double jump
+	if can_move:
+		movement() # Controls going left, right
+		doubleJump() # Controls jump and double jump
+		wallJump()
 	
 	var was_on_floor = is_on_floor()
 	# Allows movement of character
@@ -30,7 +39,9 @@ func movement():
 	# Moves player left and right
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION)
+	elif !is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, DECELERATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -50,4 +61,19 @@ func doubleJump():
 	if is_on_floor():
 		numJumps = 0
 		# pp
+
+func wallJump():
+	var direction := Input.get_axis("left", "right")
+	var direction_of_jump = -direction
+	if Input.is_action_just_pressed("jump") and is_on_wall_only() and wallJump():
+		disable_movement(0.15)
+		velocity.x = WALL_JUMP_POWER * direction_of_jump
+		velocity.y = JUMP_POWER
 		
+func disable_movement(time):
+	can_move = false
+	await get_tree().create_timer(time).timeout
+	can_move = true
+	
+func _on_PlayerDied():
+	print("player dieed")
