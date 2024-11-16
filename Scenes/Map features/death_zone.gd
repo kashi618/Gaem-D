484510a1229cc21f69e64c10c93a85fdc7fae537
osomeefm <eamonn.moore@outlook.com>
2024-11-16 18:10:34 +0000

    Jump buffer timer, deathzone
    Made the death zone a node with a variable collision shape, added a reset for player death and added jump buffer timer (hopefully this works with all the other changes added since I did this)

--- Scenes/Map features/death_zone.gd
+++ Scenes/Map features/death_zone.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 # Detects when player enters body
 func _on_body_entered(body: Node2D) -> void:
-	print("Player Died")
-	EventsBus.PlayerDied.emit()
+	if body is Player:
+		print("Player Died")
+		EventsBus.PlayerDied.emit()
