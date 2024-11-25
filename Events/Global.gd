04a3755ae9c61ea35cec2e7893c6e4027f2cd92e
Yolo-Arts <Jackteong123@gmail.com>
2024-11-25 22:12:24 +0000

    minor changes to ui and some optimisation

--- Events/Global.gd
+++ Events/Global.gd
@@ -1,9 +1,20 @@
 extends Node
 
-var spring_disabled = true
-var isFirstEncounter = true
-var hasWallPowerup = false
-var firstPlay = true
+#for testing powerups:
+var wall_jump = true
+var double_jump = true
+var hasWallPowerup = true
+var spring_disabled = false
+var isFirstEncounter = false
+var firstPlay = false
 var can_move = true
-var wall_jump = false
-var double_jump = false
+ 
+
+#for normal settings:
+#var wall_jump = false
+#var double_jump = false
+#var hasWallPowerup = false
+#var spring_disabled = true
+#var isFirstEncounter = true
+#var firstPlay = true
+#var can_move = true
